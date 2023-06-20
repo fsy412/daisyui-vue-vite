057a0f31ce0d2e260062cf4b75cab725126bc14c
fanshuangying <fsy412@gmail.com>
2023-06-20 12:08:23 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
-import { createStore } from "vuex";
-import network from "./network";
+import { createStore } from "vuex"
+import network from "./network"
 
 export default createStore({
   state() {
@@ -8,24 +8,24 @@ export default createStore({
       market: "BTC-USDT",
       quoteToken: "BTC",
       baseToken: "USDT",
-    };
+    }
   },
   getters: {
     market(state) {
-      return state.market;
+      return state.market
     },
   },
   mutations: {
     setOrderType(state, payload) {
-      state.orderType = payload.orderType;
+      state.orderType = payload.orderType
     },
     setMarket(state, payload) {
-      let market = payload.market;
-      state.market = market;
-      let arr = market.split("-");
-      state.baseToken = arr[0];
-      state.quoteToken = arr[1];
+      let market = payload.market
+      state.market = market
+      let arr = market.split("-")
+      state.baseToken = arr[0]
+      state.quoteToken = arr[1]
     },
   },
   modules: [network],
-});
+})
